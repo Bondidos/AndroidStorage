58635b2e24f6eb139098bbde84bb13cb845706f7
Bondidos <gglbond@gmail.com>
2021-12-20 15:41:54 +0300

    init

--- app/src/main/java/com/plcoding/androidstorage/MainActivity.kt
+++ app/src/main/java/com/plcoding/androidstorage/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.plcoding.androidstorage
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.plcoding.androidstorage.databinding.ActivityMainBinding
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    private fun savePhotoToInternalStorage(filename: String, bmp: Bitmap): Boolean{
+        return try{
+            openFileOutput(filename, MODE_PRIVATE).use { stream ->
+
+            }
+        }catch (e: IOException){
+            e.stackTrace
+            false
+        }
     }
 }
